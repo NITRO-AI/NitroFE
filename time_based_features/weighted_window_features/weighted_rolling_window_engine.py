1111e4da2afe1931623c597bb31a33084605c8b7
Jaswinder Singh <jaswinder9051998@gmail.com>
2021-09-15 03:15:09 +0530

    scaling down extra function

--- time_based_features/weighted_window_features/weighted_rolling_window_engine.py
+++ time_based_features/weighted_window_features/weighted_rolling_window_engine.py
@@ -55,15 +55,22 @@ class weighted_rolling_window_engine():
 
                 for _window_keys in self.payload[_column_key]['weighted_window_features'].keys():
                     if (_window_keys in self.function_mapper.keys()):
-                        self.output_dict[_column_key]['weighted_window_features'][_window_keys] = {
-                        }
-                        dict_dataframe = pd.DataFrame(
-                            self.payload[_column_key]['weighted_window_features'][_window_keys])
+
+                        self.output_dict[_column_key]['weighted_window_features'][_window_keys] = {}
+                        dict_all = self.payload[_column_key]['weighted_window_features'][_window_keys]
+
                         feature_generated = []
-                        for _iter in range(len(dict_dataframe)):
-                            resulting_feature = self.function_mapper[_window_keys](
-                                self.dataframe[_column_key], **dict_dataframe.loc[_iter].to_dict())
+                        if isinstance(self.payload[_column_key]['weighted_window_features'][_window_keys]['window'],int):
+                            iter_duration=1
+                            resulting_feature = self.function_mapper[_window_keys](self.dataframe[_column_key], **self.payload[_column_key]['weighted_window_features'][_window_keys] )
                             feature_generated.append(resulting_feature)
+
+                        else:
+                            iter_duration=len(self.payload[_column_key]['weighted_window_features'][_window_keys]['window'])
+                            for _iter in range(iter_duration):
+                                resulting_feature = self.function_mapper[_window_keys](self.dataframe[_column_key], **dict(zip(dict_all.keys(),np.array(list(dict_all.values()))[:,_iter])) )
+                                feature_generated.append(resulting_feature)
+            
                         self.output_dict[_column_key]['weighted_window_features'][_window_keys] = feature_generated
         return self.output_dict
 
@@ -74,85 +81,85 @@ payload = {'a': {'weighted_window_features':
                      'barthann': {'window': [3, 4],
                                   'min_periods': [1, 2],
                                   'symmetric': [False, True],
-                                  'operation': [np.mean, np.sum]
+                                  'operation': [np.mean, np.mean]
                                   },
                      'bartlett': {'window': [3, 4],
                                   'min_periods': [1, 2],
                                   'symmetric': [False, True],
-                                  'operation': [np.mean, np.sum]
+                                  'operation': [np.mean, np.mean]
                                   },
                      'equal':    {'window': 3,
                                   'min_periods': 1,
                                   'symmetric': False,
-                                  'operation': [np.mean]
+                                  'operation': np.mean
                                   },
-                     'blackman': {'window': [3, 6],
-                                  'min_periods': [2, 1],
+                     'blackman': {'window': [3, 3],
+                                  'min_periods': [1, 1],
                                   'symmetric': [False, True],
-                                  'operation': [np.mean, np.sum]
+                                  'operation': [np.mean, np.mean]
                                   },
-                     'blackmanharris': {'window': [3, 6],
-                                        'min_periods': [2, 1],
+                     'blackmanharris': {'window': [3, 3],
+                                        'min_periods': [1, 1],
                                         'symmetric': [False, True],
-                                        'operation': [np.mean, np.sum]
+                                        'operation': [np.mean, np.mean]
                                         },
-                     'bohman': {'window': [3, 6],
-                                'min_periods': [2, 1],
+                     'bohman': {'window': [3, 3],
+                                'min_periods': [1, 1],
                                 'symmetric': [False, True],
-                                'operation': [np.mean, np.sum]
+                                'operation': [np.mean, np.mean]
                                 },
-                     'cosine': {'window': [3, 6],
-                                'min_periods': [2, 1],
+                     'cosine': {'window': [3, 3],
+                                'min_periods': [1, 1],
                                 'symmetric': [False, True],
-                                'operation': [np.mean, np.sum]
+                                'operation': [np.mean, np.mean]
                                 },
-                     'exponential': {'window': [3, 6],
-                                     'min_periods': [2, 1],
-                                     'symmetric': [False, True],
+                     'exponential': {'window': [3, 5],
+                                     'min_periods': [1, 1],                                    
                                      'center': [1, None],
+                                     'symmetric': [False, True],
                                      'tau': [1, 0.5],
-                                     'operation': [np.mean, np.sum]
+                                     'operation': [np.mean,np.mean]
                                      },
                      'flattop': {'window': [3, 6],
                                  'min_periods': [2, 1],
                                  'symmetric': [False, True],
                                  'operation': [np.mean, np.sum]
                                  },
-                     'gaussian': {'window': [3, 6],
-                                  'min_periods': [2, 1],
+                     'gaussian': {'window': [3, 3],
+                                  'min_periods': [1, 1],
                                   'symmetric': [False, True],
                                   'std': [1, 2],
-                                  'operation': [np.mean, np.sum]
+                                  'operation': [np.mean, np.mean]
                                   },
-                    'hamming': {'window': [3, 6],
+                    'hamming': {'window': [3, 3],
                                   'min_periods': [2, 1],
                                   'symmetric': [False, True],
-                                  'operation': [np.mean, np.sum]
+                                  'operation': [np.mean, np.mean]
                                   },
-                    'hann': {'window': [3, 6],
+                    'hann': {'window': [3, 3],
                                   'min_periods': [2, 1],
                                   'symmetric': [False, True],
-                                  'operation': [np.mean, np.sum]
+                                  'operation': [np.mean, np.mean]
                                   },
-                    'kaiser': {'window': [3, 6],
+                    'kaiser': {'window': [3, 3],
                                   'min_periods': [2, 1],
                                   'symmetric': [False, True],
                                   'beta':[7,11],
-                                  'operation': [np.mean, np.sum]
+                                  'operation': [np.mean, np.mean]
                                   },
-                    'parzen': {'window': [3, 6],
+                    'parzen': {'window': [3, 3],
                                   'min_periods': [2, 1],
                                   'symmetric': [False, True],
-                                  'operation': [np.mean, np.sum]
+                                  'operation': [np.mean, np.mean]
                                   },
-                    'triang': {'window': [3, 6],
+                    'triang': {'window': [3, 3],
                                   'min_periods': [2, 1],
                                   'symmetric': [False, True],
-                                  'operation': [np.mean, np.sum]
+                                  'operation': [np.mean, np.mean]
                                   }
                  }
                  }
            }
 temp = weighted_rolling_window_engine()
 result_dict = temp.fit(df, payload)
-print(result_dict)
+print(result_dict['a']['weighted_window_features']['triang'])
