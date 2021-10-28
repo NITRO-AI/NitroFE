b9a85c011da5a5cfe3e045c1334c9a4b204eb6c6
Jaswinder Singh <jaswinder9051998@gmail.com>
2021-10-29 00:45:12 +0530

    pypi

--- NitroFE/time_based_features/indicator_features/_AverageTrueRange.py
+++ NitroFE/time_based_features/indicator_features/_AverageTrueRange.py
@@ -1,5 +1,3 @@
-
-
 import numpy as np
 import pandas as pd
 from typing import Union, Callable
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setup(name='NitroFE',
-      version='0.0.3',
+      version='0.0.5',
       url='https://github.com/NITRO-AI/NitroFE',
+      author='Nitro-AI',
+      author_email='nitro.ai.solutions@gmail.com',
       license='Apache License 2.0',
-      packages=['NitroFE'],
+      packages=['NitroFE','NitroFE.encoding.','NitroFE.time_based_features','NitroFE.time_based_features.indicator_features','NitroFE.time_based_features.moving_average_features','NitroFE.time_based_features.weighted_window_features'],
       zip_safe=True,
 	description="NitroFE is a Python feature engineering engine which provides a variety of feature engineering modules designed to handle continous calcualtion.",
       long_description=long_description  ,
@@ -14,5 +16,6 @@ setup(name='NitroFE',
 		"pandas",
 		"numpy",
 		"scipy",
-        "plotly"
-		])
+            "plotly"
+		],
+      )
