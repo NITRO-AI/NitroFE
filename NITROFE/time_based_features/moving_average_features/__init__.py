28ef6a2250b2a310725cd46e74b82551395455e2
Jaswinder Singh <jaswinder9051998@gmail.com>
2021-10-28 17:02:17 +0530

    Delete NITROFE/time_based_features directory

--- NITROFE/time_based_features/moving_average_features/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from NitroFE.time_based_features.moving_average_features.moving_average_features import ExponentialMovingFeature,HullMovingFeature,\
-    KaufmanAdaptiveMovingAverage,FractalAdaptiveMovingAverage,TripleExponentialMovingFeature,SmoothedMovingAverage
-                                            
-                                 
--- NITROFE/time_based_features/weighted_window_features/__init__.py
+++
@@ -1,4 +0,0 @@
-
-import numpy as np
-import pandas as pd
-from NitroFE.time_based_features.weighted_window_features.weighted_window_features import weighted_window_features
