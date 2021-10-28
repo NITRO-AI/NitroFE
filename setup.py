a78417f2fe5eaba7af481df0b0af188c6a88dd88
Jaswinder Singh <jaswinder9051998@gmail.com>
2021-10-28 23:19:04 +0530

    license

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(name='NitroFE',
+      version='0.0.1',
+      url='https://github.com/NITRO-AI/NitroFE',
+      license='Apache License 2.0',
+      packages=['NitroFE'],
+      zip_safe=True,
+	description="NitroFE is a Python feature engineering engine which provides a variety of feature engineering modules designed to handle continous calcualtion.",
+      long_description=long_description  ,
+	long_description_content_type='text/markdown',
+	  install_requires=[
+		"pandas",
+		"numpy",
+		"scipy",
+        "plotly"
+		],
+      )
